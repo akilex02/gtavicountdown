1a63724113bdea9ddff8d158540cc9628364f094
AkiLeX <gabrielcardoona1@gmail.com>
2026-07-09 14:34:08 -0600

    Añadir configuración de despliegue para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://akilex02.github.io',
+  base: '/gtavicountdown',
   vite: {
     plugins: [tailwindcss()]
   }
